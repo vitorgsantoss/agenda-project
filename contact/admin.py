667a289658c4e7123c54ca4ebdbf2911aaf86496
Vitor Santos <vg452004@gmail.com>
2024-12-19 13:18:33 -0300

    Add data_faker

--- contact/admin.py
+++ contact/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from contact import models
 
 # Register your models here.
+@admin.register(models.Category)
+class Category(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
+    search_fields = 'id', 'name'
+
+
 @admin.register(models.Contact)
 class Contact(admin.ModelAdmin):
     list_display = 'id','first_name', 'last_name', 'phone'
--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category (models.Model):
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Contact (models.Model):
     first_name = models.CharField(max_length=50)
@@ -12,6 +22,18 @@ class Contact (models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to= 'pictures')
+    category = models.ForeignKey(
+        Category,
+        on_delete= models.SET_NULL,
+        null= True,
+        blank= True
+        )
+    owner = models.ForeignKey(
+        User,
+        on_delete= models.SET_NULL,
+        null=True,
+        blank= True 
+        )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
