a1fc7b168a55f3d0911283d4710c333dd1dd401a
Vitor Santos <vg452004@gmail.com>
2024-12-20 12:38:42 -0300

    Função de busca por contatos implementada

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
 
 
@@ -10,7 +11,8 @@ def index(request):
     contacts = Contact.objects.filter(show = True).order_by('-id')[10:20]
     
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title' : 'Contatos - ',
         }
     
     return render(
@@ -23,13 +25,44 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(
         Contact, pk = contact_id, show = True,
         )
+    
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title,
         }
     
     return render(
         request,
         'contact/contact.html',
+        context,
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects\
+        .filter(show = True)\
+        .filter(
+            Q(first_name__icontains = search_value) |
+            Q(last_name__icontains = search_value) |
+            Q(email__icontains = search_value) |
+            Q(phone__icontains = search_value) |
+            Q(id__icontains = search_value)
+        )\
+        .order_by('-id')
+    
+    context = {
+        'contacts': contacts,
+        'site_title' : 'Contatos - ',
+        }
+    
+    return render(
+        request,
+        'contact/index.html',
         context,
     )
