3b68a46f0f9b67cfd2ad2da1535d191be910b565
Vitor Santos <vg452004@gmail.com>
2024-12-21 01:09:23 -0300

    Trabalhando nas operações de CRUD (Create)

---
+++ contact/views/contact_forms.py
@@ -0,0 +1,35 @@
+from django.core.paginator import Paginator
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, redirect
+from contact.models import Contact
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+
+def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST),
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+   
+    context = {
+        'form': ContactForm(),
+    }
+
+    return render(
+        request,
+        'contact/create.html',
+        context,
+    )
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
@@ -8,10 +9,13 @@ from contact.models import Contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(show = True).order_by('-id')[10:20]
-    
+    contacts = Contact.objects.filter(show = True).order_by('-id')
+    paginator = Paginator(contacts, 10)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title' : 'Contatos - ',
         }
     
@@ -56,8 +60,13 @@ def search(request):
         )\
         .order_by('-id')
     
+    paginator = Paginator(contacts, 10)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title' : 'Contatos - ',
         }
     
